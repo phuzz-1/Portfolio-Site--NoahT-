db9dfe0d599455802029f4dcac0691bc43447033
Noah Toews <toews.noah@gmail.com>
2024-07-12 18:58:50 -0500

    added JQuery 3.7.1 and a mobile nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello 👋, not much is happening down here, but feel free to look around.");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
